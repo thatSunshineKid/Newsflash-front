e13801151a8c3233da520ab4e0cdd5c3a9c1957e
Kurt Miller <kmiller9393@gmail.com>
2018-11-26 22:37:37 -0700

    Add Routes for SignUp and Login components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { testStore } from '../../actions';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Home from '../../components/Home/Home';
+import SignUp from '../../components/SignUp/SignUp';
+import Login from '../../components/Login/Login';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
     return (
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/login" component={Login} />
       </div>
     );
   }
@@ -23,7 +27,9 @@ export const mapDispatchToProps = dispatch => ({
   testStore: () => dispatch(testStore())
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(App)
+);
