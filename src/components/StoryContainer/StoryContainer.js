a1066c127bdf97059d843f7d1ed6acb85d7e3658
Kurt Miller <kmiller9393@gmail.com>
2018-12-12 12:55:27 -0700

    Update StoryContainer to receive stories as props from Home.js

--- src/components/StoryContainer/StoryContainer.js
+++ src/components/StoryContainer/StoryContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { mockStories } from '../../utils/mockStories';
 import StoryCard from '../StoryCard/StoryCard';
 import './StoryContainer.css';
 
 export default class StoryContainer extends Component {
   render() {
-    const storyCards = mockStories.map(item => <StoryCard {...item} />);
+    const storyCards = this.props.mockStories.map(item => (
+      <StoryCard {...item} />
+    ));
 
     return <div className="card-container">{storyCards}</div>;
   }
