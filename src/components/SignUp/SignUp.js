893e9976c28cedf800571fb926129b5691c6f0a6
Kurt Miller <kmiller9393@gmail.com>
2018-12-12 14:57:53 -0700

    Update SignUp.js classNames

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,18 +6,18 @@ import './SignUp.css';
 export default class SignUp extends Component {
   render() {
     return (
-      <div>
+      <div className="signup-container">
         <header className="signup-header">
           <NavLink to="/">
             <img className="search-logo" src={logo} alt="logo" />
           </NavLink>
         </header>
         <form className="signup-form">
-          <h2>Sign Up</h2>
+          <h2 className="form-signup-header">Sign Up</h2>
           <input type="text" placeholder="username" />
           <input type="password" placeholder="password" />
           <input type="password" placeholder="confirm password" />
-          <button>Sign Up</button>
+          <button className="signup-button">Sign Up</button>
         </form>
       </div>
     );
