8c3e272c9185841b69e6c77cf8786fe4cbcf08d3
Kurt Miller <kmiller9393@gmail.com>
2018-12-12 12:55:10 -0700

    Update StoryCard.js

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -3,13 +3,13 @@ import './StoryCard.css';
 
 export default class StoryCard extends Component {
   render() {
-    const { title, content, image } = this.props;
+    const { title, description, image } = this.props;
 
     return (
       <div>
         <h3>{title}</h3>
         <div className="card-content-container">
-          <p>{content}</p>
+          <p>{description}</p>
           <img className="card-image" src={image} alt="article" />
         </div>
       </div>
