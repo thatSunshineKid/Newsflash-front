5e4ee633d180448465d78da2bc58867fc3ab3b8f
Kurt Miller <kmiller9393@gmail.com>
2019-01-03 22:20:06 -0700

    Remove testReducer and add trendingReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import { testReducer } from './testReducer';
+import { trendingReducer } from './trendingReducer';
 
 const rootReducer = combineReducers({
-  test: testReducer
+  topStory: trendingReducer
 });
 
 export default rootReducer;
