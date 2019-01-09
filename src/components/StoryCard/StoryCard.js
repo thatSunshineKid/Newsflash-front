54c3043c3b8e26892dbecbb839fad5c3725c7207
Kurt Miller <kmiller9393@gmail.com>
2019-01-09 16:15:01 -0700

    Update StoryCard styling

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import tempImage from '../../assets/images/news-temp.jpg';
 import './StoryCard.css';
 
 export default class StoryCard extends Component {
   render() {
-    const { title, description, image } = this.props;
+    const { title, author, url, source } = this.props;
 
     return (
       <div>
-        <h3>{title}</h3>
+        <h3 className="card-title">{title}</h3>
         <div className="card-content-container">
-          <p>{description}</p>
-          <img className="card-image" src={image} alt="article" />
+          <div className="card-link-source-container">
+            <a href={url}>Article Link</a>
+            <p>
+              By: {author} at {source}
+            </p>
+          </div>
+          <img className="card-image" src={tempImage} alt="article" />
         </div>
       </div>
     );
