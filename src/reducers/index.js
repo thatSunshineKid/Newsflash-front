804be6f33b982cece741ecc1e9dc9dede40a0038
Kurt Miller <kmiller9393@gmail.com>
2019-01-10 17:37:31 -0700

    Add functionality to Login.js so user can login and add authenticationReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { postsReducer } from './postsReducer';
+import { authenticationReducer } from './authenticationReducer';
 
 const rootReducer = combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  authentication: authenticationReducer
 });
 
 export default rootReducer;
