c23af46a9a4d1126e86f7cbba368a7cc6b18514a
Kurt Miller <kmiller9393@gmail.com>
2019-01-17 15:34:22 -0700

    Add icons and update styling of StoryCard components within StoryContainer

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
-import tempImage from '../../assets/images/news-temp.jpg';
+import { NavLink, Link } from 'react-router-dom';
+import user from '../../assets/images/user-silhouette.svg';
+import favorite from '../../assets/images/star.svg';
+import chat from '../../assets/images/chat.svg';
+import authorSource from '../../assets/images/newspaper.svg';
+import like from '../../assets/images/like.svg';
+import idea from '../../assets/images/idea.svg';
 import './StoryCard.css';
 
 export default class StoryCard extends Component {
   render() {
     const { title, author, url, source } = this.props;
+    console.log(url);
 
     return (
-      <div>
+      <div className="story-card">
         <h3 className="card-title">{title}</h3>
-        <div className="card-content-container">
-          <div className="card-link-source-container">
-            <a href={url}>Article Link</a>
-            <p>
-              By: {author} at {source}
-            </p>
+        <div className="author-story-container">
+          <div>
+            <img className="card-icon" src={user} alt="author" />
           </div>
-          <img className="card-image" src={tempImage} alt="article" />
+          <a className="story-card-link" href={url}>
+            Full Story
+          </a>
+          <div className="favorite-chat-container">
+            <img
+              className="favorite-card-icon card-icon"
+              src={favorite}
+              alt="favorite"
+            />
+            <img className="card-icon" src={chat} alt="chat" />
+          </div>
+        </div>
+        <div className="source-comments-container">
+          <div>
+            <img className="card-icon" src={authorSource} alt="source" />
+          </div>
+          <NavLink className="story-card-link" to="/comments">
+            Comments
+          </NavLink>
+          <div className="like-idea-container">
+            <img className="like-card-icon card-icon" src={like} alt="like" />
+            <img className="card-icon" src={idea} alt="idea" />
+          </div>
         </div>
       </div>
     );
