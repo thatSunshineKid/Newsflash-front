1f4c8cb0bad720a0e10812a19d49361853a9bf1f
kurt miller <kmiller9393@gmail.com>
2019-02-07 19:29:34 -0700

    Add case for ADD_NEW_POST action type within postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,6 +2,8 @@ export const postsReducer = (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS':
       return [...action.posts];
+    case 'ADD_NEW_POST':
+      return [...action.posts, action.post];
     default:
       return state;
   }
